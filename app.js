93c9bd8ecd5ad551697f459e1910bea50d2827e9
BushraIrfan29 <bushrairfan2103@gmail.com>
2024-08-12 17:50:52 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//QUESTION 1
+var age = 15;
+alert("I am " + age + " years old.");
+console.log("I am " + age +" years old.");
+
+//QUESTION 2
+var visit_count;
+visit_count = 14;
+alert("You have visited this site " +visit_count + " " + " times.");
+console.log("You have visited this site " + visit_count +" times.");
+
+//QUESTION 3
+var birth_year = 2004;
+document.write("My birth year is "+birth_year + " <br/>");
+document.write("Data type of my declared variable is number" + "<br/>");
+
+//QUESTION 4
+var product_name = "T-Shirts";
+var quantity = 5;
+var visitor_name = "John Doe";
+document.write(visitor_name+" ordered "+quantity+" "+product_name+" on XYZ Clothing store");
